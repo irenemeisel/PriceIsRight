6967aa91efa739a9d66ae1c290b746755740645a
irenemeisel <irene@irenemeisel.net>
2017-01-20 02:22:35 -0500

    added model and erb and database connection

--- app.rb
+++ app.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require './models/model'
+
+
+
+
 
 get '/' do
-  "hello world"
+ erb :index
+end
+
+
+post '/submit' do
+  @model = Model.new(params[:model])
+  if @model.save
+    redirect '/models'
+  else
+    "Sorry, there was an error!"
+  end
+end
+
+
+get '/models' do
+  @models = Model.all
+  erb :models
 end
